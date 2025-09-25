820c6f072f4b61424f2b82637dc58d7e71fa0bd0
Dario Silva <dariosouzadasilva@gmail.com>
2025-09-25 10:09:52 -0300

    Inclusão de HashLocationStrategy
    Ajuste no roteamento quando o angular não é presente

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { roleAwareAuthInterceptor } from './app/interceptors/role-aware-auth.interceptor';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withInterceptors([roleAwareAuthInterceptor])),
